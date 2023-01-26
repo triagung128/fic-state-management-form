1edebda4adc283e2a1f2a0d1ce1c138e73c6b79e
triagung128 <triagung128@gmail.com>
2023-01-26 12:37:39 +0700

    upgrade dependencies

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,7 @@ import Foundation
 import file_selector_macos
 import geolocator_apple
 import package_info_plus_macos
-import path_provider_macos
+import path_provider_foundation
 import share_plus_macos
 import sqflite
 import url_launcher_macos
